2df3eb2a29277169f0dc582efc0de50466dc7d08
Mangesh <mangeshtakras777@gmail.com>
2022-11-09 21:02:38 +0530

    updated version

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/",methods= ["GET","POST"])
 def index():
-    return "my first Machine Learning Project"
+    return " Built a CI CD pipeline"
 
 if __name__ == "__main__":
     app.run(debug=True)    
